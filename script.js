84a83e6774ec5e1c2b6a8d0443186cb56e35cf2f
santiacerbis <santiacerbis2000@outlook.com>
2023-09-12 20:30:58 -0300

    Comentarios!!

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function getCharactersData() {
+        // Traigo el valor ingresado en el campo de entrada
     const characterName = document.getElementById('characterName').value;
+        // Defino la constante para la URL de la API con el nombre del personaje como parámetro de búsqueda
     const url = `https://rickandmortyapi.com/api/character/?name=${characterName}`;
-
+    // Realizo la solicitud a la API utilizando fetch
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            
+                // Verifico si hay resultados de personajes
             if (data.results.length > 0) {
+                // Tomo el primer resultado del array
                 const character = data.results[0]; 
-
+                // Creo los elementos en HTML con la información del personaje
                 const charactersData = `
                     <p><strong>Nombre:</strong> ${character.name}</p>
                     <p><strong>Estado:</strong> ${character.status}</p>
@@ -17,17 +20,18 @@ function getCharactersData() {
                     <p><strong>Ubicación:</strong> ${character.location.name}</p>
                     <img src="${character.image}" alt="${character.name}">
                 `;
-
+                // Muestro los datos del personaje en el elemento con id 'charactersData'
                 document.getElementById('charactersData').innerHTML = charactersData;
             }
         })
         .catch(error => {
+            // En caso de error agrego parrafo de informe + error en consola
             console.error('Error al obtener los datos:', error);
             document.getElementById('charactersData').innerHTML = '<p>No se encontraron resultados.</p>';
         });
 }
 function clearSearch() {
-    
+    // Borra los resultados de búsqueda previos y el campo de entrada
     document.getElementById('characterName').value = '';
     document.getElementById('charactersData').innerHTML = '';
 }
